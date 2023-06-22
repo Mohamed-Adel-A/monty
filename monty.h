bcd6e3355d7a1ea6a824f3798fe436a651650d91
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 21:53:01 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -65,6 +65,7 @@ int _isnumber(const char *str);
 void stack_push(stack_t **stack, unsigned int line_number);
 void stack_pall(stack_t **stack, unsigned int line_number);
 void stack_pint(stack_t **stack, unsigned int line_number);
+void stack_pop(stack_t **stack, unsigned int line_number);
 
 
 #endif
