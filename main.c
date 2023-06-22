8afcebad91ab8b66d80b50b75b0cfec7d1514247
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 19:12:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -45,9 +45,9 @@ ssize_t execute_line(stack_t **stack, char *line, unsigned int line_number)
 	if (!op_data.opcode || op_data.opcode[0] == '#')
 		return (0);
 	
-	for (i = 0, opfunc[i] != NULL, i++)
+	for (i = 0, opfunc[i] != NULL; i++)
 	{
-		if (strcmp(opcode, opfunc[i].opcode == 0)
+		if (strcmp(op_data.opcode, opfunc[i].opcode) == 0)
 			opfunc[i].f(stack, line_number);
 	}
 	if (opfunc[i].f == NULL)
