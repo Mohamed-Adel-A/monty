c237f7d6ac15ee7305794fb277c4d0fd65cbb7c5
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 23:38:23 +0300

    Update 6_rotl_rotr.c

--- 6_rotl_rotr.c
+++ 6_rotl_rotr.c
@@ -2,6 +2,7 @@
 
 
 void stack_rotl(stack_t **stack, unsigned int line_number);
+void stack_rotr(stack_t **stack, unsigned int line_number);
 
 /**
  * stack_rotl -  rotates the stack to the top.
@@ -31,4 +32,35 @@ void stack_rotl(stack_t **stack, unsigned int line_number)
 
 	first->next = NULL;
 	first->prev = last;
+}
+
+
+/**
+ * stack_rotr - rotates the stack to the bottom.
+ * The last element of the stack becomes the top element of the stack
+ * @stack: stack head
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void stack_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first = *stack, *last = *stack;
+
+	(void) line_number;
+	if (first == NULL || first->next == NULL)
+		return;
+
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+
+	first->prev = last;
+	(last->next)->next = NULL;
+
+	last->next = first;
+	last->prev = NULL;
+
+	*stack = last;
 }
