9a8c50ed54bba7d55386184ee700ba47ad53dd2f
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 19:58:47 +0300

    Create push.c

---
+++ push.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * _isnumber - check if string is number
+ * @str: the string to be checked
+ *
+ * Return: 1 if str is number, 0 if not
+ */
+int _isnumber(const char *str)
+{
+	int i = 0;
+
+	while (str[i] == ' ' || str[i] == '\t')
+	{
+		i++;
+	}
+
+	if (str[i] == '+' || str[i] == '-')
+	{
+		i++;
+	}
+
+	for (; str[i] != '\0' ; i++)
+	{
+		if ((str[i] < '0' || str[i] > '9'))
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
+
+
+void stack_push(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+  if (_isnumber(op_data.oparg) == 0)
+    
+	printf("%u: stack push %s\n", line_number, op_data.oparg);
+}
