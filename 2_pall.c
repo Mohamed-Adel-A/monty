5080cf9057c5257fc60d1aef487580b4f125747d
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 21:51:57 +0300

    Update 2_pall.c

--- 2_pall.c
+++ 2_pall.c
@@ -27,7 +27,13 @@ void stack_pall(stack_t **stack, unsigned int line_number)
 }
 
 
-
+/**
+ * stack_pint - prints the value at the top of the stack, followed by a new line.
+ * @stack: stack head
+ * @line_number: line number
+ *
+ * Return: void
+ */
 void stack_pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *head = *stack;
@@ -43,4 +49,30 @@ void stack_pint(stack_t **stack, unsigned int line_number)
 	}
 
 	printf("%i\n", head->n);
+}
+
+
+/**
+ * stack_pint - removes the top element of the stack.
+ * @stack: stack head
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void stack_pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack;
+
+	if (head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n",
+				line_number);
+		free(op_data.line);
+		fclose(op_data.fd);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	*stack = head->next;
+	free(head);
 }
