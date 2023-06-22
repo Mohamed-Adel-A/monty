efa0568d6d34d26c29f1a7fbf45775afd717b732
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 17:20:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,23 @@
 
 int main(int argc, char **argv)
 {
-	FILE *file;
-	char *error;
+	FILE *f;
+	char *filename;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);		
 	}
+
+	filename = argv[1];
+	f = fopen(filename, "r");
+	if(f == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	
 	(void) argv;
 	(void) file;
 	return (0);
