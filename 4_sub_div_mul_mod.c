335477776b017056a7e885c17b2c1ef6c4a4d5e7
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 22:31:39 +0300

    Update 4_sub_div_mul_mod.c

--- 4_sub_div_mul_mod.c
+++ 4_sub_div_mul_mod.c
@@ -1,10 +1,11 @@
 #include "monty.h"
 
+/**********************************************************/
 void stack_sub(stack_t **stack, unsigned int line_number);
 void stack_div(stack_t **stack, unsigned int line_number);
 void stack_mul(stack_t **stack, unsigned int line_number);
-
-
+void stack_mod(stack_t **stack, unsigned int line_number);
+/**********************************************************/
 
 
 /**
@@ -129,6 +130,55 @@ void stack_mul(stack_t **stack, unsigned int line_number)
 	second->prev = first->prev;
 
 	second->n *= first->n;
+	free(first);
+
+	*stack = second;
+}
+
+
+/**
+ * stack_mod - subtracts the top element of the stack from the second top element
+ * @stack: stack head
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void stack_mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first = *stack, *second;
+	int stack_size;
+
+	for (stack_size = 0; first != NULL ; stack_size++)
+	{
+		first = first->next;
+	}
+
+	if (stack_size < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n",
+				line_number);
+		free(op_data.line);
+		fclose(op_data.fd);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	first = *stack;
+	second = first->next;
+
+  if (first->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n",
+				line_number);
+		free(op_data.line);
+		fclose(op_data.fd);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	second->prev = first->prev;
+
+	second->n %= first->n;
 	free(first);
 
 	*stack = second;
