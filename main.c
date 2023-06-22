a29d3822443e9f6acf8cd652e5e11a83a95c904c
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 18:08:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
 #include "monty.h"
 
+
+/**
+ * stuct op_data - instuction data
+ * @line: instruction line
+ * @opcode: opcode
+ * @oparg: op argument
+ *
+ */
+typedef struct op_data
+{
+	char *line;
+	char *opcode;
+	char *oparg;	
+} op_data_t;
+
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0 success, otherwise failure
+ */
 int main(int argc, char **argv)
 {
 	FILE *fd;
-	char *filename;
+	char *filename, *line = NULL, opcode, oparg;
+	size_t line_size = 0, line_count = 1;
+	ssize_t ret_getline = 1, ret_execute_line;
 
 	if (argc != 2)
 	{
@@ -18,7 +42,46 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
-	
-	(void) argv;
+
+	for (line_count = 0; ret_getline > 0 ; line_count++)
+	{
+		ret_getline = getline(&line, &line_size, fd);
+		if (ret_getline > 0)
+		{
+			opcode = strtok(line, " \n");
+			if (opcode[0] == "#")
+			{
+				free(line);
+				continue;
+			}
+			opfunction = get_op_function();
+			ret_execute_line = execute_line();
+			if (ret_execute_line == -1)
+			{
+				fprintf()
+			}
+		}
+		free(line);		
+	}
+
 	return (0);
 }
+
+execute_line()
+{
+	int i = 0;
+	opcode_functions_t opfunc[] = { {"push", stack_push},
+					{"pall", stack_pull},
+					{"pint", stack_pint),
+					{NULL, NULL}
+					};
+
+	for (i = 0, opfunc[i] != NULL, i++)
+	{
+		if (strcmp(opcode, opfunc[i].opcode == 0)
+			return (opfunc[i].opfunction);
+	}
+	return (NULL);
+
+}
+int get_op_function()()
