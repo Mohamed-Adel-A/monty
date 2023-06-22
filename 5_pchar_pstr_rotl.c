a9ecd85ff815a859b1960474f80aa16fd2b29eb0
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 22:44:29 +0300

    Create 5_pchar_pstr_rotl.c

---
+++ 5_pchar_pstr_rotl.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+void stack_pchar(stack_t **stack, unsigned int line_number);
+
+
+/**
+ * stack_pchar -  prints the char at the top of the stack.
+ * @stack: stack head
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void stack_pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack;
+
+	if (head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n",
+				line_number);
+		free(op_data.line);
+		fclose(op_data.fd);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	if (head->n < 0 || head->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n",
+				line_number);
+		free(op_data.line);
+		fclose(op_data.fd);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", head->n);
+}
