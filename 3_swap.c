d10d50771d1c31b222825255027a1a749119c3f1
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 22:15:32 +0300

    Update 3_swap.c

--- 3_swap.c
+++ 3_swap.c
@@ -40,3 +40,42 @@ void stack_swap(stack_t **stack, unsigned int line_number)
 
 	*stack = second;
 }
+
+
+/**
+ * stack_add - adds the top two elements of the stack.
+ * @stack: stack head
+ * @line_number: line number
+ *
+ * Return: void
+ */
+void stack_add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *first = *stack, *second;
+	int stack_size;
+
+	for (stack_size = 0; first != NULL ; stack_size++)
+	{
+		first = first->next;
+	}
+
+	if (stack_size < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n",
+				line_number);
+		free(op_data.line);
+		fclose(op_data.fd);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	first = *stack;
+	second = first->next;
+
+	second->prev = first->prev;
+
+	second->n += first->n;
+	free(first)
+
+	*stack = second;
+}
