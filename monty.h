c520a66b3f03608cc72a6bb58a0effeba981bc1b
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2023-06-22 22:31:45 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -72,6 +72,8 @@ void stack_nop(stack_t **stack, unsigned int line_number);
 void stack_sub(stack_t **stack, unsigned int line_number);
 void stack_div(stack_t **stack, unsigned int line_number);
 void stack_mul(stack_t **stack, unsigned int line_number);
+void stack_mod(stack_t **stack, unsigned int line_number);
+
 
 
 #endif
